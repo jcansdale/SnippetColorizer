938397fa997c39589b56f9b5a64943e776c14703
Jamie Cansdale <jcansdale@github.com>
2018-11-07 09:37:09 +0000

    Add project files.

---
+++ ColorizedTextFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.LanguageServices;
+
+namespace SnippetColorizer
+{
+    [Export]
+    public class ColorizedTextFactory : Workspace
+    {
+        readonly ITextBufferFactoryService textBufferFactory;
+        readonly ITextEditorFactoryService editorFactory;
+        readonly IContentTypeRegistryService contentTypeRegistry;
+        readonly object miscellaneousFilesWorkspace;
+
+        [ImportingConstructor]
+        public ColorizedTextFactory(
+            VisualStudioWorkspace visualStudioWorkspace,
+            ITextBufferFactoryService textBufferFactory,
+            ITextEditorFactoryService editorFactory,
+            IContentTypeRegistryService contentTypeRegistry,
+            [Import("Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.MiscellaneousFilesWorkspace")]
+        object miscellaneousFilesWorkspace)
+            : base(visualStudioWorkspace.Services.HostServices, WorkspaceKind.MiscellaneousFiles)
+        {
+            this.textBufferFactory = textBufferFactory;
+            this.editorFactory = editorFactory;
+            this.contentTypeRegistry = contentTypeRegistry;
+            this.miscellaneousFilesWorkspace = miscellaneousFilesWorkspace;
+        }
+
+        public IWpfTextViewHost CreateTextViewHost(string source, string contentType)
+        {
+            var textView = CreateTextView(source, contentType);
+            return editorFactory.CreateTextViewHost(textView, false);
+        }
+
+        public IWpfTextView CreateTextView(string source, string contentType)
+        {
+            var textBuffer = CreateTextBuffer(source, contentType);
+            return editorFactory.CreateTextView(textBuffer);
+        }
+
+        public ITextBuffer CreateTextBuffer(string source, string contentType)
+        {
+            var ct = contentTypeRegistry.GetContentType(contentType);
+            var textBuffer = textBufferFactory.CreateTextBuffer(source, ct);
+            var projectInfo = CreateProjectInfoForDocument(@"x:\moniker.cs");
+            OnProjectAdded(projectInfo);
+            var sourceTextContainer = textBuffer.AsTextContainer();
+            OnDocumentOpened(projectInfo.Documents.Single().Id, sourceTextContainer);
+            return textBuffer;
+        }
+
+        ProjectInfo CreateProjectInfoForDocument(string filePath)
+        {
+            return (ProjectInfo)miscellaneousFilesWorkspace.GetType()
+                .GetMethod("CreateProjectInfoForDocument", BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(miscellaneousFilesWorkspace, new object[] { filePath });
+        }
+    }
+}
---
+++ ColorizedTextFactoryAdHocTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Diagnostics;
+using System.ComponentModel.Composition;
+
+namespace SnippetColorizer
+{
+    [Export]
+    class ColorizedTextFactoryAdHocTests
+    {
+        readonly ColorizedTextFactory colorizedTextFactory;
+
+        [ImportingConstructor]
+        ColorizedTextFactoryAdHocTests(ColorizedTextFactory colorizedTextFactory)
+        {
+            this.colorizedTextFactory = colorizedTextFactory;
+        }
+
+        [STAThread]
+        void ShowTextViewHost()
+        {
+            var source = GetSource();
+            var host = colorizedTextFactory.CreateTextViewHost(source, "CSharp");
+            var window = new Window { Content = host.HostControl };
+            window.ShowDialog();
+        }
+
+        [STAThread]
+        void ShowTextView()
+        {
+            var source = GetSource();
+            var view = colorizedTextFactory.CreateTextView(source, "CSharp");
+            var window = new Window { Content = view };
+            window.ShowDialog();
+        }
+
+        static string GetSource() =>
+            File.ReadAllText(new StackFrame(true).GetFileName());
+    }
+}
